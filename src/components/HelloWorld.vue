0766d4a6d9e0a97b55a4c8f9430cc450305359f6
sskrtt <sskrtt@users.noreply.github.com>
2019-10-06 19:13:20 +0900

    10/6の進捗 永田レビュー用

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,62 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div>
+    {{ msg }}
+    <form>
+      <button v-on:click="addGuest()">ADD Guest</button>
+      <button>Remove Guests</button>
+      <p>新しいguestの情報を入力して ADD Guestボタンを押してください
+      <p>name: <input type="text" v-model="newName"> name of guest: {{newName}}</p>
+      <p>sex: <input type="text" v-model="newSex"> sex of guest: {{newSex}}</p>
+      <p>room: <input type="number" v-model="newRoom"> room of guest: {{newRoom}}</p>
+    </form>
+ <div class="guest-list">
+      <label class="guest-list__item"
+             v-for="guest in guests">
+        <p>
+        <input type="checkbox">
+        <button>EDIT</button>
+        名前は{{ guest.name }}、 性別は {{guest.sex}}、 RoomNumberは {{guest.room}}です。
+        </p>
+      </label>
+    </div>  </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      guests : [
+        {name : 'John', sex: `male`,room:150},
+        {name : 'Mary', sex: `female`,room:153},
+        {name : 'Ryo', sex: `male`,room:165},
+      ],
+    newName:"",
+    newSex:"",
+    newRoom:""
+    }
+  },
+  methods:{
+    addGuest:function(event){
+      let checkName = this.newName && this.newName.trim();
+      let checkSex = this.newSex && this.newSex.trim();
+      let checkRoom = this.newRoom && this.newRoom.trim();
+
+      if(!checkName & !checkSex & !checkRoom){
+        alert `入力されていない項目があります`;
+      }
+      this.guests.push({
+        name:newName,
+        sex:newSex,
+        room:newRoom
+      })
+    }
+  }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
 </style>
